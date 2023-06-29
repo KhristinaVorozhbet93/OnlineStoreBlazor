662219c0a45467e80015ca37ecf8a753081188ca
Voro_to54 <Voro_to54@itstepru.onmicrosoft.com>
2023-06-29 22:18:57 +0400

    Добавлены стили для страницы

--- OnlineStoreBlazor/Program.cs
+++ OnlineStoreBlazor/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using OnlineStoreBlazor;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
