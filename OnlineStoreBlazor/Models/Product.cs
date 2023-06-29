968194a82a56a2e14c6d93364a51635044996070
Voro_to54 <Voro_to54@itstepru.onmicrosoft.com>
2023-06-29 21:00:21 +0400

    Добавьте файлы проекта.

---
+++ OnlineStoreBlazor/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineStoreBlazor.Models
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name)) throw new NullReferenceException(nameof(name));
+
+            Name = name;
+            Price = price;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ProducedAt { get; set; }
+        public DateTime ExpiredAt { get; set; }
+    }
+}
