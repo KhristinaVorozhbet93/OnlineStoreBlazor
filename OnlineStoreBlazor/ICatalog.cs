66e9024fae545244a37a192fa02dc671e75fc06b
Voro_to54 <Voro_to54@itstepru.onmicrosoft.com>
2023-06-29 21:01:32 +0400

    Добавлен интерфейс ICatalog

---
+++ OnlineStoreBlazor/ICatalog.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStoreBlazor.Models;
+
+namespace OnlineStoreBlazor
+{
+    public interface ICatalog
+    {
+        List<Product> GetProducts();
+        void AddProduct(Product product);
+        Product GetProduct(Guid id);
+        void DeleteProduct(Guid productId);
+        void UpdateProduct(Guid productId, Product newProduct);
+        void ClearCatalog();
+    }
+}
